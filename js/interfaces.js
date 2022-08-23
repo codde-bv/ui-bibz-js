94ce8a126c2ac1cb7e918495ced06ecffcd760d7
Shanti Bouchez-Mongardé <shanti-pub.git@mildred.fr>
2022-08-23 15:50:15 +0200

    Change how bootstrap is imported

--- js/interfaces.js
+++ js/interfaces.js
@@ -1,6 +1,6 @@
 // Import non minified version, later minifier will make sure minified version
 // is provided, while still allowing debugging to use the non minified version.
-import bootstrap from "bootstrap/dist/js/bootstrap.bundle"
+import {Tab, Popover, Tooltip, Dropdown, Toast} from "bootstrap/dist/js/bootstrap.bundle"
 
 export default class UiBibzInterface {
 
@@ -17,7 +17,7 @@ export default class UiBibzInterface {
 
   nav(){
     this.node.querySelectorAll('.nav-tabs .nav-item .nav-link').forEach(function(el){
-      let tabTrigger = new bootstrap.Tab(el)
+      let tabTrigger = new Tab(el)
       el.addEventListener('click', function (e) {
         e.preventDefault()
         tabTrigger.show()
@@ -26,19 +26,19 @@ export default class UiBibzInterface {
   }
 
   popover() {
-    this.node.querySelectorAll('[data-bs-toggle="popover"]').forEach(x => new bootstrap.Popover(x))
+    this.node.querySelectorAll('[data-bs-toggle="popover"]').forEach(x => new Popover(x))
   }
 
   tooltip() {
-    this.node.querySelectorAll('[data-bs-toggle="tooltip"]').forEach(x => new bootstrap.Tooltip(x))
+    this.node.querySelectorAll('[data-bs-toggle="tooltip"]').forEach(x => new Tooltip(x))
   }
 
   dropdown() {
-    this.node.querySelectorAll('[data-bs-toggle="dropdown"]').forEach(x => new bootstrap.Dropdown(x))
+    this.node.querySelectorAll('[data-bs-toggle="dropdown"]').forEach(x => new Dropdown(x))
   }
 
   toast() {
-    this.node.querySelectorAll('.toast').forEach(x => new bootstrap.Toast(x))
+    this.node.querySelectorAll('.toast').forEach(x => new Toast(x))
   }
 
   notify() {
